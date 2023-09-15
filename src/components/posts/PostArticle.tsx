8e5b2645b8d33aa86ca34ca16106f08fbcb75e50
tehseen <tehseen.type@gmail.com>
2023-09-15 16:40:23 +0530

    feat - added delete post functionality

--- src/components/posts/PostArticle.tsx
+++ src/components/posts/PostArticle.tsx
@@ -2,9 +2,20 @@
 
 import { TPost } from "@/lib/types";
 
-import { User } from "@nextui-org/react";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  User,
+  useDisclosure,
+} from "@nextui-org/react";
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 import React from "react";
 
 import ReactMarkdown from "react-markdown";
@@ -18,9 +29,38 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import Comments from "../comments/Comments";
 
 import moment from "moment";
-import Image from "next/image";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useAppSelector } from "@/hooks/reduxHooks";
+import { useMutation } from "@tanstack/react-query";
 
 const PostArticle = ({ post }: { post: TPost }) => {
+  const router = useRouter();
+
+  const { isOpen, onOpenChange, onOpen, onClose } = useDisclosure();
+
+  const { user } = useAppSelector((state) => state.auth);
+
+  const deletePost = async () => {
+    const { data } = await axios.delete(
+      `/api/posts/${post.path}?id=${post.id}`
+    );
+    return data;
+  };
+
+  const { mutate, isLoading } = useMutation(deletePost, {
+    onSuccess: (data) => {
+      toast.success(data.message);
+      console.log(data);
+      onClose();
+      router.push("/");
+    },
+    onError: (data: any) => {
+      toast.error(data.message);
+      console.log(data);
+    },
+  });
+
   return (
     <>
       <article className="pb-4">
@@ -36,22 +76,43 @@ const PostArticle = ({ post }: { post: TPost }) => {
               />
             </figure>
           )}
-          <User
-            name={post.author.name}
-            as={Link}
-            href={`/${post.author.username}`}
-            description={
-              <div className="text-default-500">
-                Posted on: (
-                {moment(post.updatedAt, moment.ISO_8601).format("DD MMM")} ){" "}
-                {moment(post.updatedAt, moment.ISO_8601, "DDMMMYYYY").fromNow()}
+          <div className="flex justify-between items-center">
+            <User
+              name={post.author.name}
+              as={Link}
+              href={`/${post.author.username}`}
+              description={
+                <div className="text-default-500">
+                  Posted on: (
+                  {moment(post.createdAt, moment.ISO_8601).format("DD MMM")} ){" "}
+                  {moment(
+                    post.createdAt,
+                    moment.ISO_8601,
+                    "DDMMMYYYY"
+                  ).fromNow()}
+                </div>
+              }
+              avatarProps={{
+                src: `${post.author.avatar}`,
+              }}
+              className=""
+            />
+            {user && user.id === post.author.id ? (
+              <div>
+                <Button variant="light" className="mr-2" size="sm">
+                  Edit
+                </Button>
+                <Button
+                  variant="light"
+                  color="danger"
+                  size="sm"
+                  onClick={onOpen}
+                >
+                  Delete
+                </Button>
               </div>
-            }
-            avatarProps={{
-              src: `${post.author.avatar}`,
-            }}
-            className=""
-          />
+            ) : null}
+          </div>
           <h1 className="mb-6 mt-4 scroll-m-20 lg:text-5xl md:text-4xl text-3xl sm:font-extrabold font-bold tracking-tight">
             {post.title}
           </h1>
@@ -71,6 +132,44 @@ const PostArticle = ({ post }: { post: TPost }) => {
       </article>
       <hr className="pb-8" />
       <Comments post={post} />
+
+      {/* ===DELETE MODAL=== */}
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        placement="center"
+        radius="sm"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader>
+                Are you sure you want to delete this post?
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  You cannot undo this action, perhaps you just want to edit
+                  instead?
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  color="danger"
+                  onPress={() => mutate()}
+                  radius="sm"
+                  isDisabled={isLoading}
+                  isLoading={isLoading}
+                >
+                  {isLoading ? "Deleting" : "Yes, Delete"}
+                </Button>
+                <Button onPress={onClose} radius="sm">
+                  No, Cancel
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
   );
 };
